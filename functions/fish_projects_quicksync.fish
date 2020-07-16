ca18a63aceaeeb811f23c63f908eddb6e976e80d
Andrew Matthews <andrew.matthews@team.telstra.com>
2020-07-16 10:07:04 +1000

    wip

--- functions/fish_projects_quicksync.fish
+++ functions/fish_projects_quicksync.fish
@@ -1,4 +1,4 @@
-function fish_projects_sync -d "commits any changes in current porject home dir to git and push to origin etc"
+function fish_projects_quicksync -d "commits any changes in current porject home dir to git and push to origin etc"
   fish_projects_sync "wip"
 end
 
