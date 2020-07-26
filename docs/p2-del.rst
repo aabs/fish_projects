9a5d685d5a3bc4ba0898511cc88fe9c38f4c8572
Andrew Matthews <andrew.matthews@team.telstra.com>
2020-07-27 09:32:56 +1000

    wip

---
+++ docs/p2-del.rst
@@ -0,0 +1,2 @@
+p2 del -s ee
+p2 add -i 3010 -s p2 -t 'fish projects' -d 'new incarnation of prject tool' -p (pwd)
--- functions/__fd2_p2_open.fish
+++ functions/__fd2_p2_open.fish
@@ -1,7 +1,7 @@
 function __fd2_p2_open -d "select from existing projects"
   __fd2_p2_select 
-  if set -q __fd2_p2_selected 
-    __fd2_p2_goto -s $__fd2_p2_selected
+  if set -q __fd2_p2_current_sn 
+    __fd2_p2_goto -s $__fd2_p2_current_sn
   end
 end
 
