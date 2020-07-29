1cf58c681d924802cf80526813fc31db5261fc4c
Andrew Matthews <andrew.matthews@team.telstra.com>
2020-07-29 22:00:29 +1000

    ripple thru edits from fd2

--- functions/__fd2_p2_cur.fish
+++ functions/__fd2_p2_cur.fish
@@ -1,5 +1,5 @@
 function __fd2_p2_cur -d "display name of current project"
   set -l p (__fd2_p2_sql_retrieve_project -s $__fd2_p2_current_sn)
   set -l title (__fd2_p2_get_project_field -f title -r $p)
-__fd2_colour_print bryellow "$__fd2_p2_current_sn: "; echo $title
+  fd2_colour_print bryellow "$__fd2_p2_current_sn: "; echo $title
 end
