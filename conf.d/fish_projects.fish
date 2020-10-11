803ff9792ae7383da1e356a4bc7fdd6a37ac14d4
Andrew Matthews <matthews.andrew@gmail.com>
2020-10-12 10:49:25 +1100

    different keybinding that doesn't clash on macos

--- conf.d/fish_projects.fish
+++ conf.d/fish_projects.fish
@@ -1,9 +1,9 @@
 set -q XDG_CONFIG_HOME || set -x XDG_CONFIG_HOME $HOME/.config
 
-bind \co __fd2_p2_open
+bind \cP __fd2_p2_open
 
 set -l name (basename (status -f) .fish){_uninstall}
 
 function $name --on-event $name
-    bind --erase \co
+    bind --erase \cP
 end
